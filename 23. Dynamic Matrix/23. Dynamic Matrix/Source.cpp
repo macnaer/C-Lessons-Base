6aa2b1bbdb699aa6ac1af2d24d36a62c7873d38c
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2019-09-19 12:58:34 +0300

    Good example

--- 23. Dynamic Matrix/23. Dynamic Matrix/Source.cpp
+++ 23. Dynamic Matrix/23. Dynamic Matrix/Source.cpp
@@ -6,31 +6,34 @@ using namespace std;
 int main() {
 	srand(unsigned(time(NULL)));
 
-	int col, row;
+	int col = 0, row = 0;
 	cout << "Enter matrix size: [row first] => " << endl;
 	cin >> row >> col;
 
 	int **arr = new int *[row];
 
+	for (int i = 0; i < row; i++) {		
+		arr[i] = new int[col];
+	}
+
+
 	for (int i = 0; i < row; i++) {
 		for (int j = 0; j < col; j++) {
-			int *arr = new int[row];
+			arr[i][j] = rand() % 10 + 1;
 		}
 	}
 
 	for (int i = 0; i < row; i++) {
 		for (int j = 0; j < col; j++) {
-			cout << arr[i][j] << endl;
+			cout << arr[i][j] << "\t";
 		}
+		cout << endl;
 	}
 
 
-	//for (int i = 0; i < row; i++) {
-	//	for (int j = 0; j < col; j++) {
-	//		delete[] arr;
-	//	}
-	//}
-
+	for (int i = 0; i < row; i++) {
+		delete[] arr[i];
+	}
 
 	delete[] arr;
 
