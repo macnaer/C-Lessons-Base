6c0cdb50265e2140fb66b742538712b6d8fdefd4
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2019-09-26 12:58:33 +0300

    String

--- 25. String/25. String/Source.cpp
+++ 25. String/25. String/Source.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
+#include <string>
 
 
 using namespace std;
 
 int main() {
 
-	char name[5] = {"Bill"};
+	/*char name[5] = {"Bill"};
 	char hello[6] = {"Hello"};
 	char result[255] = {};
 
-	cout << strlen(hello) << endl;
+	cout << strlen(hello) << endl;*/
 	//strcat_s(result, hello);
 	//strcat_s(result, " ");
 	//strcat_s(result, name);
 	//cout << result << endl;
 
-	cout << "Name" << name << endl;
+	/*cout << "Name" << name << endl;*/
 
+	string personName = "Bill";
+	string surname = " Gates ";
+	string hello = "Hello mr. ";
 
+	string res = hello + "\n" + personName + "\n" + surname;
+
+	//cout << hello << personName << surname << endl;
+
+	//cout << res << endl;
+
+	string strArr[4] = {"Bill", "Stive", "Tom", "Jack"};
+
+	for (int i = 0; i < 4; i++)
+	{
+		cout << strArr[i] << endl;
+	}
 
 	system("pause");
 	return 0;
