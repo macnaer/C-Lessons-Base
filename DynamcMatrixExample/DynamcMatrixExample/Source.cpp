17c0f01e0489b863d074c4f87f4060d772d9aadf
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2019-10-01 13:11:46 +0300

    DynamicMatrix Example + string example

--- DynamcMatrixExample/DynamcMatrixExample/Source.cpp
+++ DynamcMatrixExample/DynamcMatrixExample/Source.cpp
@@ -46,13 +46,45 @@ int main() {
 	srand(unsigned(time(NULL)));
 	int **matrix = NULL;
 	int col = 0, row = 0;
-	cout << "Enter row and col =>" << endl;
-	cin >> row >> col;
+	//cout << "Enter row and col =>" << endl;
+	//cin >> row >> col;
 
-	CreateArr(matrix, row, col);
-	FillArr(matrix, row, col);
-	PrintArr(matrix, row, col);
-	DeleteArr(matrix, row);
+	//CreateArr(matrix, row, col);
+	//FillArr(matrix, row, col);
+	//PrintArr(matrix, row, col);
+	//DeleteArr(matrix, row);
+
+	char symbols[255] = {};
+	char words[255] = {};
+
+	int coordsSIZE = 0;
+	int coordsCounter = 0;
+	
+	cout << "Enter symbols ==>>>" << endl;
+	cin >> symbols;
+
+	for (int i = 0; i < strlen(symbols); i++) {
+		/*cout << symbols[i] << endl;*/
+		if (int(symbols[i]) == 95) {
+			cout << symbols[i] << endl;
+			coordsSIZE++;
+		}
+	}
+
+	int *coords = new int[coordsSIZE];
+
+	for (int i = 0; i < strlen(symbols); i++) {
+		if (int(symbols[i]) == 95) {
+			coords[coordsCounter] = i;
+			coordsCounter++;
+		}
+	}
+
+	for (int i = 0; i < coordsSIZE; i++) {
+		cout << "Underscore coorda = " << coords[i] << endl;
+	}
+
+
 
 	system("pause");
 	return 0;
